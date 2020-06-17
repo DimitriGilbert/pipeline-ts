9e2a2f7ba1ce0fd448148840be955b55ba4c08db
Dimitri Gilbert <dimitri.gilbert@gmail.com>
2020-06-17 13:48:09 +0200

    forgot to build ^^'

--- build/index.js
+++ build/index.js
@@ -2,9 +2,18 @@
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./Event"));
 __export(require("./Log"));
 __export(require("./Payload"));
 __export(require("./Pipeline"));
 __export(require("./Stage"));
+exports.Fs = __importStar(require("./fs"));
+exports.Http = __importStar(require("./http"));
